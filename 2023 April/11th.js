acf75e7b22c97d505ac3f65f1858f5aa507bb536
Emin Tahirov <emintahirov1996@gmail.com>
2023-04-11 12:46:55 -0700

    solution

--- 2023 April/11th.js
+++ 2023 April/11th.js
@@ -1,1 +1,39 @@
-console.log(12%10)
+const array1 = [7, 4, 3];
+const array2 = [5, 6, 4];
+
+const list1 = {
+  val: 3,
+  next: {
+    val: 4,
+    next: {
+      val: 7,
+      next: null,
+    },
+  },
+};
+
+const list2 = {
+  val: 4,
+  next: {
+    val: 6,
+    next: {
+      val: 5,
+      next: null,
+    },
+  },
+};
+
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+var addTwoNumbers = function (l1, l2, carry) {
+  if (!l1 && !l2 && !carry) return null;
+
+  var total = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + (carry || 0);
+  carry = parseInt(total / 10);
+  return new ListNode(total % 10, addTwoNumbers(l1?.next, l2?.next, carry));
+};
+
+console.log(addTwoNumbers(list1, list2));
