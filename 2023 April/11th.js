5bb17f4b2d20299332a1d73298d8273a2a3f579f
Emin Tahirov <emintahirov1996@gmail.com>
2023-04-11 13:43:47 -0700

    cleanin up linked list

--- 2023 April/11th.js
+++ 2023 April/11th.js
@@ -43,7 +43,7 @@ var addTwoNumbers = function (l1, l2, carry) {
   if (!l1 && !l2 && !carry) return null;
 
   var total = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + (carry || 0);
-  carry = parseInt(total / 10);
+  carry = Math.trunc(total / 10);
   return new ListNode(total % 10, addTwoNumbers(l1?.next, l2?.next, carry));
 };
 
