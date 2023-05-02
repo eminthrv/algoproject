6d41beff61eeb81a180f83c981321e40708a0285
Emin Tahirov <emintahirov1996@gmail.com>
2023-05-02 14:27:00 -0600

    solved problem

--- 2023 May/may2/may2.js
+++ 2023 May/may2/may2.js
@@ -1,11 +1,27 @@
-
-
 const uncompress = (s) => {
-    let string = "";
-    
-    for (let i = 0; i < s.length; i++){
-      
-    }
-  };
+  let splitString = [];
+  let tempStr = '';
+  let result = '';
 
-  
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+
+    if (isNaN(char)) {
+      // Check if the character is a letter
+      tempStr += char;
+      splitString.push(tempStr);
+      tempStr = '';
+    } else {
+      tempStr += char;
+    }
+  }
+
+  for (const el of splitString) {
+    let letter = el[el.length - 1];
+    let number = Number(el.replace(/\D+/g, ''));
+    result += letter.repeat(number);
+  }
+  return result;
+};
+
+console.log(uncompress('3n12e2z'));
