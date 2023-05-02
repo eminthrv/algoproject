eb71dfc46cf7b256a3b6ced1930799f7bcc2354d
Emin Tahirov <emintahirov1996@gmail.com>
2023-05-02 16:21:19 -0600

    added problem 4

--- 2023 May/may2/may2pt3.js
+++ 2023 May/may2/may2pt3.js
@@ -6,14 +6,34 @@ function digitalRoot(n) {
     var digits = n.toString().split('');
     console.log(digits);
     var realDigits = digits.map(Number);
-    console.log(realDigits)
+    console.log(realDigits);
     return digitalRoot(realDigits.reduce((partialSum, a) => partialSum + a, 0));
   }
 }
 
-const performance = require('perf_hooks').performance
+const performance = require('perf_hooks').performance;
 const startTime = performance.now();
 const result = digitalRoot(493193);
-const endTime = performance.now()
-console.log("Result:", result);
-console.log("Time taken:", endTime - startTime, "milliseconds");
+const endTime = performance.now();
+console.log('Result:', result);
+console.log('Time taken:', endTime - startTime, 'milliseconds');
+
+function digitalRootxyz(n) {
+  if (n < 10) {
+    return n;
+  }
+
+  var digits = n.toString().split('');
+  console.log(digits);
+  var realDigits = digits.map(Number);
+  console.log(realDigits);
+  n = realDigits.reduce((partialSum, a) => partialSum + a, 0);
+
+  return digitalRootxyz(n);
+}
+
+const startTime1 = performance.now();
+const result1 = digitalRootxyz(493193);
+const endTime1 = performance.now();
+console.log('Result:', result1);
+console.log('Time taken:', endTime1 - startTime1, 'milliseconds');
---
+++ 2023 May/may2/may2pt4.js
@@ -0,0 +1,4 @@
+const compress = (s) => {
+    // todo
+  };
+  
