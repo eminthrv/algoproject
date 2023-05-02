562e04eae06f828604233c73ec8f9a9ba2f181c1
Emin Tahirov <emintahirov1996@gmail.com>
2023-05-02 16:55:12 -0600

    added problem 4

--- 2023 May/may2/may2pt4.js
+++ 2023 May/may2/may2pt4.js
@@ -1,4 +1,33 @@
 const compress = (s) => {
-    // todo
-  };
-  
+  let splitString = [];
+  let tempStr = '';
+  let output = '';
+
+  for (let i = 0; i < s.length; i++) {
+    if (i === 0) {
+      tempStr += s[i];
+    }
+    if (i > 0 && s[i - 1] === s[i]) {
+      tempStr += s[i];
+    }
+    if (i > 0 && s[i - 1] !== s[i]) {
+      splitString.push(tempStr);
+      tempStr = s[i];
+    }
+    if (s[i + 1] === undefined) {
+      splitString.push(tempStr);
+      tempStr = s[i];
+    }
+  }
+  console.log(splitString);
+  for (e of splitString) {
+    let length = e.length.toString();
+    let stringElement = length + e[0];
+    output += stringElement;
+  }
+  return output;
+};
+
+console.log(compress('annneseeeeeeeeeeezzffffssa'));
+//console.log(compress('nnneeeeeeeeeeeezz'));
+//console.log(compress('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'));
